ab108c11e97e909e0952f228aa1c4174e02cf4c9
Alexander Egorenkov <aegorenk@gmail.com>
2015-08-24 23:32:32 -0400

    Add test select sepal length should match dplyr

--- udpy/tests/test_select.py
+++ udpy/tests/test_select.py
@@ -1,9 +1,23 @@
 from unittest import TestCase
 
-from udpy.select import select
+import udpy
+import os
 
+DIRECTORY = os.getcwd()
+SEP = os.sep
 
 class TestSelect(TestCase):
 
-    def test_new_case(self):
-        select("Not a proper file")
+    def test_select_sepal_length_should_match_dplyr(self):
+        test_file_path = [DIRECTORY, 'udpy', 'tests', 'data',
+                          'iris_select_sepal_length.csv']
+        test_file_path = SEP.join(test_file_path)
+
+        with open(test_file_path) as test_file:
+            test_lines = [line for line in test_file.readlines()]
+
+        file_path = [DIRECTORY, 'udpy', 'tests', 'data', 'iris_test.csv']
+        file_path = SEP.join(file_path)
+
+        lines = udpy.select(file_path, 'sepal_length')
+        self.assertEqual(lines, test_file)
